4c4fb43f20939e5ddd87cd639d0ffa1ade8f8b2f
rujuu <81368541+Rujuu-prog@users.noreply.github.com>
2023-03-12 03:25:36 +0900

    :zap: add title & subTitle props

--- packages/app/components/CardCustom.tsx
+++ packages/app/components/CardCustom.tsx
@@ -3,16 +3,25 @@ import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
 import React, { ReactNode, useState } from 'react'
 import { useLink } from 'solito/link'
 
-  export function CardCustom(props: CardProps, children:ReactNode) {
+type Props = {
+  title: string
+  subTitle: string
+  url: string
+}
+
+  export function CardCustom<Props>({title, subTitle, url}) {
+    const linkProps = useLink({
+      href: url,
+    })
     return (
-      <Card theme="dark" elevate size="$4" bordered {...props}>
+      <Card theme="dark" elevate size="$4" bordered hoverStyle={{ scale: 0.925 }} pressStyle={{ scale: 0.875 }} animation="bouncy" w={250} h={300} scale={0.9}>
         <Card.Header padded>
-          <H2>Sony A7IV</H2>
-          <Paragraph theme="alt2">Now available</Paragraph>
+          <H2>{title}</H2>
+          <Paragraph theme="alt2">{subTitle}</Paragraph>
         </Card.Header>
         <Card.Footer padded>
           <XStack f={1} />
-          <Button br="$10">Purchase</Button>
+          <Button br="$10" {...linkProps}>Try</Button>
         </Card.Footer>
         <Card.Background>
           
