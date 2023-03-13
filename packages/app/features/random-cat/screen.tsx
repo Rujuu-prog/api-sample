66554e7a11c5ab54f7f053d48087dbc3b06d2c0e
rujuu <81368541+Rujuu-prog@users.noreply.github.com>
2023-03-13 19:45:03 +0900

    :bug: fix type error at random-cat

--- packages/app/features/random-cat/screen.tsx
+++ packages/app/features/random-cat/screen.tsx
@@ -1,13 +1,27 @@
 import { Anchor, Button, H1, Input, Paragraph, Separator, Sheet, XStack, YStack, Card, Image, H2, CardProps } from '@my/ui'
-import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
-import { CardCustom } from 'app/components/CardCustom'
-import React, { useState } from 'react'
-import { useLink } from 'solito/link'
+import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons';
+import { CardCustom } from 'app/components/CardCustom';
+import React, { useState } from 'react';
+import { useLink } from 'solito/link';
+import useSWR, { mutate } from 'swr';
+import axios from "axios";
+
+interface Data {
+  data: {file: string};
+}
+
 
 export function RandomCatScreen() {
+    const { data, error } = useSWR<Data>('https://aws.random.cat/meow', axios);
+    const getCatImg = () => {
+      mutate('https://aws.random.cat/meow', false)
+      console.log('called');
+    }
     return (
-      <XStack $sm={{ flexDirection: 'column' }} px="$4" space>
-        <p>random cat api</p>
-      </XStack>
+      <YStack $sm={{ flexDirection: 'column' }} px="$4" space>
+        <H1>random cat api</H1>
+        {data&&<Image src={data.data.file} width={200} height={300} />}
+        <Button onPress={()=>(getCatImg())}>Change Image</Button>
+      </YStack>
     )
   }
