96386b643481ef3bba0696e12e99d01f8b892782
rujuu <81368541+Rujuu-prog@users.noreply.github.com>
2023-03-14 11:49:55 +0900

    :sparkles: add side menu

--- packages/app/components/layout/SideList.tsx
+++ packages/app/components/layout/SideList.tsx
@@ -1,40 +1,40 @@
 import { XStack, YGroup, ListItem } from '@my/ui'
-import { Search, Monitor, Bell, Settings } from '@tamagui/lucide-icons'
+import { Flag, Heart, Bell, Settings } from '@tamagui/lucide-icons'
 import { Link } from 'solito/link'
 
 export function SideList(){
     return(
-        <div >
-            <YGroup als="center" bordered w={240} size="$4">
-            <YGroup.Item>
-                <Link href="/">
-                <ListItem style={{borderRadius: '9px 9px 0 0'}} hoverTheme icon={Search}>
-                        Search
-                </ListItem>
+        // <SContainer>
+        // </SContainer>
+            <YGroup als="center" bordered w={240} size="$4" >
+                <YGroup.Item>
+                    <Link href="/">
+                    <ListItem borderTopRightRadius="9px" borderTopLeftRadius="9px" hoverTheme icon={Flag}>
+                            Home
+                    </ListItem>
+                    </Link>
+                </YGroup.Item>
+                <YGroup.Item>
+                <Link href="/favorite">
+                    <ListItem hoverTheme icon={Heart}>
+                        Favorite
+                    </ListItem>
                 </Link>
-            </YGroup.Item>
-            <YGroup.Item>
-            <Link href="/screen">
-                <ListItem hoverTheme icon={Monitor}>
-                Screen
-                </ListItem>
-            </Link>
-            </YGroup.Item>
-            <YGroup.Item>
-            <Link href="/test">
-                <ListItem hoverTheme icon={Bell}>
-                Bell
-                </ListItem>
-            </Link>
-            </YGroup.Item>
-            <YGroup.Item>
-            <Link href="/test">
-                <ListItem style={{borderRadius: '0 0 9px 9px'}} hoverTheme icon={Settings}>
-                Settings
-                </ListItem>
-            </Link>
-            </YGroup.Item>
+                </YGroup.Item>
+                <YGroup.Item>
+                <Link href="/bell">
+                    <ListItem hoverTheme icon={Bell}>
+                    Bell
+                    </ListItem>
+                </Link>
+                </YGroup.Item>
+                <YGroup.Item>
+                <Link href="/settings">
+                    <ListItem borderBottomRightRadius="9px" borderBottomLeftRadius="9px" hoverTheme icon={Settings}>
+                    Settings
+                    </ListItem>
+                </Link>
+                </YGroup.Item>
             </YGroup>
-        </div>
     )
 }
--- packages/app/provider/navigation/index.web.tsx
+++ packages/app/provider/navigation/index.web.tsx
@@ -1,9 +1,8 @@
 // on Web, we don't use React Navigation, so we avoid the provider altogether
 // instead, we just have a no-op here
 // for more, see: https://solito.dev/recipes/tree-shaking
-import { XStack, YGroup, ListItem, Button } from '@my/ui'
+import { XStack, YGroup, ListItem, Button, YStack } from '@my/ui'
 import { SideList } from 'app/components/layout/SideList'
-import styled from "styled-components";
 
 export const NavigationProvider = ({
   children,
@@ -11,18 +10,15 @@ export const NavigationProvider = ({
   children: React.ReactElement
 }) => {
   return(
-    <SContainer>
-      <XStack $sm={{ flexDirection: 'column' }} px="$4" space>
-        {children}
-      </XStack>
-    </SContainer>
+    <XStack position='relative'>
+      <YStack flex={1} maxWidth="300px" position="absolute" top="90px" left="30px">
+        <SideList />
+      </YStack>
+      <YStack flex={2} marginLeft="300px">
+        <XStack $sm={{ flexDirection: 'column' }} px="$4" space>
+          {children}
+        </XStack>
+      </YStack>
+    </XStack>
   )
 }
-
-const SContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  height: 100vh;
-`;
-
-
